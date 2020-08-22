4966c8cd36185ba961abd4c2848eeb0d1cb5a7c8
Christian <alvescastro00cca13@gmail.com>
2020-08-22 10:33:13 -0300

    Restructuring server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const server = express()
 
+
+
+function pageLanding(req, res){
+    return res.sendFile(__dirname + '/views/index.html')
+}
+
+function pageStudy(req, res){
+    return res.sendFile(__dirname + '/views/study.html')
+}
+
+function pageGiveClasses(req, res){
+    return res.sendFile(__dirname + '/views/give-classes.html')
+}
+
 server.use(express.static('public'))
 
-server.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/views/index.html')
-})
+server.get('/', pageLanding)
 
-server.get('/study', (req, res) => {
-    return res.sendFile(__dirname + '/views/study.html')
-})
+server.get('/study', pageStudy)
 
-server.get('/give-classes', (req, res) => {
-    return res.sendFile(__dirname + '/views/give-classes.html')
-})
+server.get('/give-classes', pageGiveClasses)
 
 server.listen(5500)
